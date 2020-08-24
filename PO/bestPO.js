93d99bef60cb457a9d195cc5b4318da2b2e98161
Irina.Gerasimovich <iryna.herasimovich@exposit.com>
2020-08-24 18:01:40 +0300

    24.08 added sliders

--- PO/bestPO.js
+++ PO/bestPO.js
@@ -4,13 +4,38 @@ let BestPage;
 
 BestPage = function() {
     let data = require('../data/data.json'),
+        headline = element(by.xpath("//H1[@class='home home_headline'][text()='Навигатор по лучшим фильмам']"));
         genreList = element(by.id('genreListTitle')),
-        genreItemDrama = element(by.className('genre_8 selectItem')),
-        //ratingRangeSliderLeft = element(by.xpath("[//A[@class='ui-slider-handle ui-state-default ui-corner-all']"));
-        ratingRangeSliderLeft1 = element(by.id("ratingrange")).element(by.xpath("//A[@class='ui-slider-handle ui-state-default ui-corner-all']"));
+        genreItemDrama = element(by.className('genre_8 selectItem')).element(by.name('m_act[genre][]')),
+        //ratingRangeSliderLeft = element(by.id("ratingrange")).all(by.xpath("//A[@class='ui-slider-handle ui-state-default ui-corner-all']")).get(0),
+        //ratingRangeSliderRigth = element(by.id("ratingrange")).all(by.xpath("//A[@class='ui-slider-handle ui-state-default ui-corner-all']")).get(2),
+
+        ratingRangeSliderLeft = element(by.xpath("//DIV[@id='ratingrange']")).all(by.xpath("//A[@class='ui-slider-handle ui-state-default ui-corner-all']")).get(0),
+        //moves second slider of the first block 'ratingrange' - ???
+        ratingRangeSliderRigth = element(by.xpath("//DIV[@id='ex_ratingrange']")).all(by.xpath("//A[@class='ui-slider-handle ui-state-default ui-corner-all']")).get(0),
+
+
+        //ratingRangeSliderLeft = element(by.id("ratingrange")).element(by.xpath("//a[not(contains(@style, '100%'))]")),
+        //ratingRangeSliderRigth = element(by.id("ratingrange")).element(by.xpath("//a[contains(@style, '100%')]")),
+
+        ratingImdbLeft = element(by.id("ex_ratingrange")).all(by.xpath("//A[@class='ui-slider-handle ui-state-default ui-corner-all']")).get(0),
+        ratingMin = element(by.id('rating_min'));
+        ratingMax = element(by.id('rating_max'));
+        btnShowFilms = element(by.xpath("//INPUT[@class='nice_button']")),
+        searchResultsLabel = element(by.className('level2')),
+        itemList = element(by.id('itemList')),
+        firstFilm = element(by.className('name')).element(by.cssContainingText("Побег из Шоушенка"));;
+
+        this.returnHeadline = function() {
+            return headline;
+        };
         
         this.openGenreList = function() {
             genreList.click();
+        };
+
+        this.returnDramaGenre = function() {
+            return genreItemDrama;
         };
 
         this.selectDramaGenre = function() {
@@ -18,12 +43,48 @@ BestPage = function() {
         };
 
         this.moveRatingLeftSlider = function() {
-            browser.actions().dragAndDrop(ratingRangeSliderLeft1,{x:-70,y:0}).perform(); 
+            browser.actions().dragAndDrop(ratingRangeSliderLeft,{x:-70,y:0}).perform(); 
+        };
+
+        this.moveRatingRigthSlider = function() {
+            browser.actions().dragAndDrop(ratingRangeSliderRigth,{x:-45,y:0}).perform(); 
+        };
+
+        this.moveratingImdbLeft = function() {
+            browser.actions().dragAndDrop(ratingImdbLeft,{x:-70,y:0}).perform();
         };
 
         this.ratingRangeSliderLeftIsVisible = function() {
             return ratingRangeSliderLeft;
-        }
+        };
+
+        this.returnRatingMin = function() {
+            return ratingMin;
+        };
+
+        this.returnRatingMax = function() {
+            return ratingMax;
+        };
+
+        this.returnBtnShowFilms = function() {
+            return btnShowFilms;
+        };
+
+        this.btnShowFilmsClick = function() {
+            btnShowFilms.click();
+        };
+
+        this.returnResultLabel = function() {
+            return searchResultsLabel;
+        };
+
+        this.returnItemList = function() {
+            return itemList;
+        };
+
+        this.returnFirstFilm = function() {
+            return firstFilm;
+        };
 
 };
 
--- specs/best-search.spec.js
+++ specs/best-search.spec.js
@@ -2,7 +2,7 @@ let BestPage = require('../PO/bestPO.js');
 const { browser } = require('protractor');
 //const { ExpectedConditions, browser } = require('protractor');
 
-describe('Best film searching', () => {
+describe('Best film searching:', () => {
     let bestPage = new BestPage(),
         data = require('../data/data.json'),
         EC = protractor.ExpectedConditions;
@@ -11,32 +11,46 @@ describe('Best film searching', () => {
         browser.waitForAngularEnabled(false);
     });
 
-    it('Open best search page', async() => {
+    it('1) open best search page', async() => {
         await browser.get(data.bestFilmsUrl);
+
+        expect(await bestPage.returnHeadline().isPresent()).toBeTruthy();
     });
 
-    it('Fill search criteria', async() => {
+    it('2) fill genre', async() => {
        await bestPage.openGenreList();
-       await bestPage.selectDramaGenre();
+       await bestPage.selectDramaGenre(); 
 
-       
-       //let ratingRangeSliderLeft = element(by.xpath("//A[@class='ui-slider-handle ui-state-default ui-corner-all']"));
-       let ratingRangeSliderLeft1 = element(by.id("ratingrange")).element(by.xpath("//A[@class='ui-slider-handle ui-state-default ui-corner-all']"));
-       await browser.wait(EC.visibilityOf(ratingRangeSliderLeft1), 10000);
-
-       await bestPage.moveRatingLeftSlider();
-       
-       let btnShowFilms = element(by.className("keywordsPopup")).element(by.cssContainingText('value', 'показать фильмы'));
-       browser.wait(EC.visibilityOf(btnShowFilms), 15000);
-       btnShowFilms.click();
-       browser.sleep(5000);
-
-       //let firstFilm = element(by.cssContainingText("Побег из Шоушенка"));
-
-       //await browser.wait(EC.visibilityOf(firstFilm), 10000);
-
-       //expect(firstFilm.getText()).toBe("Побег из Шоушенка");
+       expect(bestPage.returnDramaGenre().isSelected()).toBe(true);
     });
+
+    it('3) fill rating', async() => { 
+       await browser.wait(EC.visibilityOf(bestPage.ratingRangeSliderLeftIsVisible()), 10000);
+       
+       await bestPage.moveRatingLeftSlider();
+       await browser.wait(EC.textToBePresentInElementValue(bestPage.returnRatingMin(), '3.1'), 50000);   
+       
+       await bestPage.moveRatingRigthSlider();
+       await browser.wait(EC.textToBePresentInElementValue(bestPage.returnRatingMax(), '8.5'), 50000); 
+
+       //await bestPage.moveratingImdbLeft();
+       
+       expect(await bestPage.returnRatingMin().getAttribute('value')).toEqual('3.1');
+       expect(await bestPage.returnRatingMax().getAttribute('value')).toEqual('8.5');
+    });
+
+    it('4) show found films', async() => {
+       browser.wait(EC.visibilityOf(bestPage.returnBtnShowFilms()), 50000);
+       await bestPage.btnShowFilmsClick();
+       
+       browser.wait(EC.visibilityOf(bestPage.returnResultLabel()), 50000);
+       expect(await bestPage.returnResultLabel().isPresent()).toBe(true);
+      });
+
+    /*it('5) list of films', async() => {
+       browser.wait(EC.visibilityOf(bestPage.returnItemList()), 20000);
+       expect(bestPage.returnFirstFilm().getText()).toBe("Побег из Шоушенка");
+    });*/
 
 })
 
