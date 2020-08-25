0aaf71be6bf257f56cc0209eb34d18b55ac7c610
Irina.Gerasimovich <iryna.herasimovich@exposit.com>
2020-08-25 17:50:37 +0300

    added range, years, age (without expect)

--- PO/bestPO.js
+++ PO/bestPO.js
@@ -4,32 +4,45 @@ let BestPage;
 
 BestPage = function() {
     let data = require('../data/data.json'),
-        headline = element(by.xpath("//H1[@class='home home_headline'][text()='Навигатор по лучшим фильмам']"));
-        genreList = element(by.id('genreListTitle')),
-        genreItemDrama = element(by.className('genre_8 selectItem')).element(by.name('m_act[genre][]')),
-        //ratingRangeSliderLeft = element(by.id("ratingrange")).all(by.xpath("//A[@class='ui-slider-handle ui-state-default ui-corner-all']")).get(0),
-        //ratingRangeSliderRigth = element(by.id("ratingrange")).all(by.xpath("//A[@class='ui-slider-handle ui-state-default ui-corner-all']")).get(2),
+        headline = element(by.xpath("//H1[@class='home home_headline'][text()='Навигатор по лучшим фильмам']")),
 
+        //Жанры и страны
+        genreList = element(by.id('genreListTitle')),
+        genreExcludeList = element(by.id('exclude_genreListTitle')),
+        genreItemDrama = element(by.id('genreList')).element(by.className('genre_8 selectItem')).element(by.name('m_act[genre][]')),
+        genreItemExcludeGame = element(by.id('exclude_genreList')).element(by.className('genre_27 selectItem')).element(by.name('m_act[egenre][]')),
+        
+        //Годы создания
+        yearIntervalMin = element(by.name('m_act[years][min]')),
+        yearIntervalMax = element(by.name('m_act[years][max]')),
+        yearMin = element(by.name('m_act[years][min]')).element(by.cssContainingText('option', '2010')),
+        yearMax = element(by.name('m_act[years][max]')).element(by.cssContainingText('option', '2018')),
+
+        //Рейтинги и возрастные ограничения
         ratingRangeSliderLeft = element(by.xpath("//DIV[@id='ratingrange']")).all(by.xpath("//A[@class='ui-slider-handle ui-state-default ui-corner-all']")).get(0),
         //moves second slider of the first block 'ratingrange' - ???
         ratingRangeSliderRigth = element(by.xpath("//DIV[@id='ex_ratingrange']")).all(by.xpath("//A[@class='ui-slider-handle ui-state-default ui-corner-all']")).get(0),
-
-
-        //ratingRangeSliderLeft = element(by.id("ratingrange")).element(by.xpath("//a[not(contains(@style, '100%'))]")),
-        //ratingRangeSliderRigth = element(by.id("ratingrange")).element(by.xpath("//a[contains(@style, '100%')]")),
-
         ratingImdbLeft = element(by.id("ex_ratingrange")).all(by.xpath("//A[@class='ui-slider-handle ui-state-default ui-corner-all']")).get(0),
-        ratingMin = element(by.id('rating_min'));
-        ratingMax = element(by.id('rating_max'));
+        minVoteRange = element(by.id('num_voterange')).all(by.xpath("//A[@class='ui-slider-handle ui-state-default ui-corner-all']")).get(8),
+        minVoteField = element(by.id('min_vote')),
+        ratingMin = element(by.id('rating_min')),
+        ratingMax = element(by.id('rating_max')),
+        ageRestriction = element(by.name('m_act[restriction]')),
+        ageRestriction18  = element(by.name('m_act[restriction]')).element(by.cssContainingText('option', '18+')),
+
+        //Бюджет и кассовые сборы
+
+        //Поиск и результаты
         btnShowFilms = element(by.xpath("//INPUT[@class='nice_button']")),
         searchResultsLabel = element(by.className('level2')),
         itemList = element(by.id('itemList')),
-        firstFilm = element(by.className('name')).element(by.cssContainingText("Побег из Шоушенка"));;
+        firstFilm = element(by.className('name')).element(by.cssContainingText("Побег из Шоушенка"));
 
         this.returnHeadline = function() {
             return headline;
         };
         
+        //Жанры и страны
         this.openGenreList = function() {
             genreList.click();
         };
@@ -42,6 +55,36 @@ BestPage = function() {
             genreItemDrama.click();
         };
 
+        this.openExcludeGenreList = function() {
+            genreExcludeList.click();
+        };
+
+        this.returnExcludeGameGenre = function() {
+            return genreItemExcludeGame;
+        };
+
+        this.selectExcludeGameGenre = function() {
+            genreItemExcludeGame.click();
+        };
+
+
+        //Годы создания
+        this.selectMinYear = function() {
+            yearIntervalMin.click();
+            yearMin.click();
+        };
+
+        this.selectMaxYear = function() {
+            yearIntervalMax.click();
+            yearMax.click();
+        };
+
+        this.returnMinYear = function() {
+            return yearIntervalMax;
+        };
+
+
+        //Рейтинги и возрастные ограничения
         this.moveRatingLeftSlider = function() {
             browser.actions().dragAndDrop(ratingRangeSliderLeft,{x:-70,y:0}).perform(); 
         };
@@ -65,6 +108,30 @@ BestPage = function() {
         this.returnRatingMax = function() {
             return ratingMax;
         };
+
+        this.returnMinVoteRangeSlider = function() {
+            return minVoteRange;
+        };
+
+        this.returnMinVoteRangeField = function() {
+            return minVoteField;
+        };
+
+        this.setMinVoteRange = function() {
+            browser.actions().dragAndDrop(minVoteRange,{x:70,y:0}).perform();
+        };
+
+        this.setAgeRestriction = function() {
+            ageRestriction.click();
+            ageRestriction18.click();
+        };
+
+        this.returnAgeRestruction = function() {
+            return ageRestriction;
+        };
+
+
+        //Поиск и результаты
 
         this.returnBtnShowFilms = function() {
             return btnShowFilms;
--- specs/best-search.spec.js
+++ specs/best-search.spec.js
@@ -20,7 +20,7 @@ describe('Best film searching:', () => {
     it('2) fill genre', async() => {
        await bestPage.openGenreList();
        await bestPage.selectDramaGenre(); 
-
+            
        expect(bestPage.returnDramaGenre().isSelected()).toBe(true);
     });
 
@@ -39,18 +39,57 @@ describe('Best film searching:', () => {
        expect(await bestPage.returnRatingMax().getAttribute('value')).toEqual('8.5');
     });
 
-    it('4) show found films', async() => {
-       browser.wait(EC.visibilityOf(bestPage.returnBtnShowFilms()), 50000);
+    it('4) set year interval', async() => {
+        await bestPage.selectMinYear();
+        await bestPage.selectMaxYear();
+       
+        //expect(await bestPage.returnMinYear().getText()).toEqual('2010');
+        //expect(await bestPage.().getAttribute('value')).toEqual('1245');
+
+     });
+
+    it('5) fill exclude genre', async() => {
+       await bestPage.openExcludeGenreList();
+       await bestPage.selectExcludeGameGenre();
+       
+       expect(await bestPage.returnExcludeGameGenre().isSelected()).toBe(true);
+     }); 
+
+    it('6) set minimal vote range', async() => {
+         browser.wait(EC.visibilityOf(bestPage.returnMinVoteRangeSlider()), 20000);
+         await bestPage.setMinVoteRange();
+         browser.sleep(5000);
+         
+         await browser.wait(EC.textToBePresentInElementValue(bestPage.returnMinVoteRangeField(), '1245'), 20000); 
+         expect(bestPage.returnMinVoteRangeField().getAttribute('value')).toEqual('1245');
+     }); 
+
+    it('7) set age restriction', async() => {
+        await bestPage.setAgeRestriction();
+        
+        await browser.wait(EC.textToBePresentInElementValue(bestPage.returnAgeRestruction(), '18+'), 20000);
+        
+        bestPage.returnAgeRestruction().getAttribute()
+        .then(value => {
+            console.log(value)
+        });
+
+        //expect(bestPage.returnAgeRestruction().getAttribute('value')).toEqual('18+');
+     });
+
+    it('8) show found films', async() => {
+       browser.wait(EC.visibilityOf(bestPage.returnBtnShowFilms()), 20000);
        await bestPage.btnShowFilmsClick();
        
-       browser.wait(EC.visibilityOf(bestPage.returnResultLabel()), 50000);
+       browser.wait(EC.visibilityOf(bestPage.returnResultLabel()), 20000);
        expect(await bestPage.returnResultLabel().isPresent()).toBe(true);
       });
 
-    /*it('5) list of films', async() => {
+    it('9) list of films', async() => {
        browser.wait(EC.visibilityOf(bestPage.returnItemList()), 20000);
-       expect(bestPage.returnFirstFilm().getText()).toBe("Побег из Шоушенка");
-    });*/
+       //expect(bestPage.returnFirstFilm().getText()).toBe("Побег из Шоушенка");
+       
+    });
 
 })
 
